edc8969b113859512d9cce3b5da57e26522b40aa
Edwin Ssekabira <66394421+djswoosh@users.noreply.github.com>
2021-12-09 00:29:51 +0000

    Update Swooshstreamspy.py

--- Swooshstreamspy.py
+++ Swooshstreamspy.py
@@ -32,7 +32,7 @@ with correlation:
 		df = pd.read_csv('https://github.com/djswoosh/Music-Recommendation-Engine-using-FMA-Dataset/blob/400c6b577c5a1855765caec98243e437bda3106a/swooshstreams.csv')
 		export_file = 'swooshstreams.html'
 		df.save_as_html(export_file)
-		txt = Path(export_file.read_text()
+		txt = Path(export_file).read_text()
 		components.html(txt, width=800, height=350)
 
 	app()
